3b2c71d386e2388d1c684163169c5ee889214ec6
Juan Camilo Florez Paez <jcflorezpaez@gmail.com>
2025-03-25 16:50:59 -0500

    feat: se genera la primera integración con la API para el landing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import { Box, Divider, Grid2, Stack, Typography } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Divider, Grid2, IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { useEvents } from '../hooks/useEvents'
+
+import ShareIcon from '@mui/icons-material/Share';
+import InfoIcon from '@mui/icons-material/Info';
+import { useCurrency } from '../hooks/useCurrency';
 
 export const Home = () => {
+
+    const { events } = useEvents()
+    const { formatter } = useCurrency()
 
     return (
         <Box minHeight={'100vh'} width={'100%'} sx={{ overflow: 'hidden' }}>
@@ -58,8 +66,51 @@ export const Home = () => {
                 </Grid2>
             </Box>
 
-            <Box>
+            <Box sx={{
+                maxWidth: '1500px',
+                minHeight: '50vh',
+                my: 2,
+                px: 2,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                mx: 'auto',
+            }}>
+                {
+                    events.length > 0 && events.map((event, index) => (
+                        <Card sx={{ width: 400 }} key={index}>
+                            <CardMedia
+                                sx={{ height: 240 }}
+                                image="/placeholder.webp"
+                                title="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {event.name}
+                                </Typography>
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    {event.description}
+                                </Typography>
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    {formatter(event.price)}
+                                </Typography>
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    {event.location}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <IconButton color='primary' aria-label="share">
+                                    <ShareIcon />
+                                </IconButton>
 
+                                <IconButton color='primary' aria-label="info">
+                                    <InfoIcon />
+                                </IconButton>
+
+                            </CardActions>
+                        </Card>
+                    ))
+                }
             </Box>
 
         </Box >
