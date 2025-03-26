d73d635f50affbef236c12e9b6dac09416ba5401
Juan Camilo Florez Paez <jcflorezpaez@gmail.com>
2025-03-26 14:07:52 -0500

    feat: se genera la integración del token en los pedidos de la API

--- src/helpers/conectateApi.js
+++ src/helpers/conectateApi.js
@@ -8,6 +8,14 @@ const conectateApi = axios.create({
         "Content-Type": "application/json",
     },
 });
-//conectateApi.interceptors.request.use((config) => { })
+
+conectateApi.interceptors.request.use((config) => {
+    const {token} = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')) : {};
+    console.log(token)
+    if (token) {
+        config.headers['token'] = token;
+    }
+    return config;
+})
 
 export default conectateApi;
